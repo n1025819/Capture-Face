8c6a481d3907d9eea278499eede0f4abcdf29949
Shi,ChengYou <45199828+uuboyscy@users.noreply.github.com>
2019-01-11 17:28:37 +0800

    Add files via upload

---
+++ ConfusionMatrixAccuracy.py
@@ -0,0 +1,86 @@
+# coding:utf-8
+import cv2
+import os
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+print('Loading model file...')
+recognizer.read('./trainer/trainer.yml')
+# cascadePath = "haarcascade_frontalface_default.xml"
+#faceCascade = cv2.CascadeClassifier(cascadePath)
+
+# model
+face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface_default.xml')
+
+# font style
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# iniciate id counter
+id = 0
+
+#
+os.mkdir('./tester/tptn')
+tptn = 0
+all = 0
+checkId = 0
+
+# names related to ids: example ==> JieCheng-Pan: id=1,  etc
+names = ['None', 'JieCheng-Pan', 'MinQi-Qiu', 'ShengZhi-Qiu', 'ChengYou-Shi', 'YoHeng-Guo', 'YoQing-Chen', 'LvAn-Lin',
+         'JunLong-chen', 'YuZHe-Qiu', 'YiYing-Chen']
+
+testerPath = r'./tester'
+for user in os.listdir(testerPath):
+    print('user ', user)
+    imgCount = 0
+    for img in os.listdir(testerPath + '/' + user):
+        # image path
+        imagepath = testerPath + '/' + user + '/' + img
+        print(imagepath)
+
+        # read image
+        image = cv2.imread(imagepath)
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+        # fine face in image
+        faces = face_cascade.detectMultiScale(gray,
+                                              scaleFactor = 1.2,
+                                              minNeighbors = 5,
+                                              minSize = (5,5))
+
+        print("There are %s faces on the photo!"%(str(len(faces))))
+
+        for(x,y,w,h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+
+            # Check if confidence is less them 100 ==> "0" is perfect match
+            if (confidence < 100):
+                checkId = id
+                id = names[id]
+                confidence = "  %s" % (round(100 - confidence))
+            else:
+                id = "unknown"
+                confidence = "  %s" % (round(100 - confidence))
+
+            cv2.putText(image, str(id), (x + 5, y - 5), font, 3, (255, 255, 255), 5)
+            cv2.putText(image, str(confidence), (x + 5, y + h - 5), font, 3, (255, 255, 0), 5)
+            # if TP/TN -> save the photo
+            try:
+                if int(checkId) == int(user):
+                    cv2.imwrite('./tester/tptn/%s.%s.%s.jpg'%(id, imgCount, confidence), image)
+                    tptn += 1
+            except:
+                print('stop!')
+
+        imgCount += 1
+
+        print('id=',id)
+        print('confidence=',confidence)
+        print('-')
+        all += 1
+        res = cv2.resize(image, (600, 800), interpolation=cv2.INTER_CUBIC)
+        cv2.imshow("Find Faces!",res)
+        cv2.waitKey(1)
+
+print('\nTP and TN : %s'%tptn)
+print('Data amount : %s'%all)
+print('ACC : %s'%(tptn/all))
---
+++ FaceModeling_v1.py
@@ -0,0 +1,115 @@
+# coding:utf-8
+'''
+1. Capture the face in each photo.
+2. Save face photoes into ./dataset.
+3. Maybe there are some mistakes while cutting photoes , so the improper photoes will be removed.
+4. After removing , there maybe exist a few improper photoes that could not be detected , so the process will pause for 5 minutes so that you can remove those photoes manually.
+5. Train model.
+'''
+import cv2
+import os
+import numpy as np
+from PIL import Image
+import time
+
+## Cut photoes and save into /dataset/
+if not os.path.isdir('./dataset'):
+    os.mkdir('./dataset')
+
+dtUserDir = os.listdir('./Face')
+try:
+    userId = int(os.listdir('./dataset')[len(os.listdir('./dataset')) - 1].split('.')[1]) + 1
+except:
+    userId = 1
+    print('[INFO] There is no data in /dataset')
+print('[INFO] New user ID will start from %s' % userId)
+
+for user in range(userId - 1, len(dtUserDir)):
+    dt = os.listdir('./Face/%s' % (dtUserDir[user]))
+    count = 1
+    for face in dt:
+        # image path
+        imagepath = r'./Face/%s/%s' % (dtUserDir[user], face)
+        print(imagepath)
+
+        # model
+        face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface_default.xml')
+
+        # read image
+        image = cv2.imread(imagepath)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # find face in image
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        print("[INFO] There are %s faces on the photo!" % (str(len(faces))))
+
+        for (x, y, w, h) in faces:
+            # cv2.circle(image,((x+x+w)/2,(y+y+h)/2),w/2,(0,255,0),2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.imwrite("./dataset/User." + str(userId) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+
+        count += 1
+
+    userId += 1
+
+print('[INFO] Face photoes cut!')
+print('[INFO] Removint improper photoes.')
+time.sleep(3)
+
+## Filter the photo -> remove improper photoes
+datasetPath = r'./dataset'
+
+for i in os.listdir(datasetPath):
+    print(i)
+    size = os.path.getsize(datasetPath + '/' + i)
+    print(size)
+    if size < 100000:
+        os.remove(datasetPath + '/' + i)
+        print('Deleted!')
+
+print('[INFO] Improper photoes removed.')
+print('[INFO] Please check if there still exist improper photoes in your directory manually.')
+print('[INFO] Model training will start 5 min later.')
+time.sleep(300)
+
+## Training
+# Path for face image database
+path = './dataset'
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+# function to get the images and label data
+def getImagesAndLabels(path):
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    faceSamples=[]
+    ids = []
+
+    for imagePath in imagePaths:
+
+        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+        print('%s is modeling...'%imagePath)
+
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+
+    return faceSamples,ids
+
+print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+print('[INFO] Modling all data...')
+# Save the model into trainer/trainer.yml
+os.mkdir('./trainer')
+recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+# Print the numer of faces trained and end program
+print("\n [INFO] %s faces trained. Exiting Program"%(len(np.unique(ids))))
